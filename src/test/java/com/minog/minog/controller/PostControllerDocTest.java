807cf767f272c96cc5878dd32a8467c2a1123081
pmh5574 <g_a_m_e_@naver.com>
2023-04-07 15:33:45 +0900

    spring boot Restdocs 설정

---
+++ src/test/java/com/minog/minog/controller/PostControllerDocTest.java
@@ -0,0 +1,12 @@
+package com.minog.minog.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerDocTest {
+
+
+}
