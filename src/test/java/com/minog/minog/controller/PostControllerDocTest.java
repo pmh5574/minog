7b61789a82d277c273f2746c436aef649367c70b
pmh5574 <g_a_m_e_@naver.com>
2023-04-08 19:36:31 +0900

    Autowired 오류..

--- src/test/java/com/minog/minog/controller/PostControllerDocTest.java
+++ src/test/java/com/minog/minog/controller/PostControllerDocTest.java
@@ -1,9 +1,12 @@
 package com.minog.minog.controller;
 
+import com.minog.minog.domain.Post;
+import com.minog.minog.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -15,6 +18,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -22,6 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PostControllerDocTest {
 
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -31,11 +38,19 @@ public class PostControllerDocTest {
     }
 
     @Test
-    @DisplayName("아무개")
+    @DisplayName("글 단건 조회 테스트")
     void test1() throws Exception {
+        // given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
         // expected
-        this.mockMvc.perform(get("/")
+        this.mockMvc.perform(get("/posts/{postId}", 1L)
                         .accept(APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andDo(document("index"));
     }
