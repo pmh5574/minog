c6aa9028225b9088a30e6585a57fa444e841f197
pmh5574 <g_a_m_e_@naver.com>
2023-04-13 17:17:07 +0900

    글 작성 만들기

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,11 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import 'bootstrap/dist/css/bootstrap-utilities.css'
+
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
