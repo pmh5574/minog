8b1856367ff6fbeff1e80c7fcfd66e030f7196e4
pmh5574 <g_a_m_e_@naver.com>
2023-05-31 17:05:33 +0900

    jwt 적용 중 값이 제대로 안넘어온다..

--- src/main/java/com/minog/minog/config/SpringSecurityConfig.java
+++ src/main/java/com/minog/minog/config/SpringSecurityConfig.java
@@ -1,58 +1,56 @@
 package com.minog.minog.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
 
-//    private final JwtTokenProvider jwtTokenProvider;
-
-//    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
-//        this.jwtTokenProvider = jwtTokenProvider;
-//    }
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .defaultSuccessUrl("/", true)
+//                .permitAll()
+//                .and()
+//                .logout();
+//
         http
                 .httpBasic().disable()
                 .csrf().disable() // httpBasic().disable().csrf().disable(): rest api이므로 basic auth 및 csrf 보안을 사용하지 않는다는 설정
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT를 사용하기 때문에 세션을 사용하지 않는다는 설정
                 .and()
                 .authorizeRequests()
-                .antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**").permitAll() // 해당 API에 대해서는 모든 요청을 허가한다는 설정
+                .antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**", "/auth/login", "/").permitAll() // 해당 API에 대해서는 모든 요청을 허가한다는 설정
                 .anyRequest().authenticated() // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); //JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행하겠다는 설정
         return http.build();
     }
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .defaultSuccessUrl("/", true)
-                    .permitAll()
-                .and()
-                    .logout();
-    }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
-    }
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
+//    }
 }
--- src/main/java/com/minog/minog/service/AuthService.java
+++ src/main/java/com/minog/minog/service/AuthService.java
@@ -1,12 +1,17 @@
 package com.minog.minog.service;
 
+import com.minog.minog.config.JwtTokenProvider;
 import com.minog.minog.domain.Session;
+import com.minog.minog.domain.TokenInfo;
 import com.minog.minog.domain.User;
 import com.minog.minog.exception.InvalidSigninInformation;
 import com.minog.minog.repository.UserRepository;
 import com.minog.minog.request.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,6 +28,8 @@ import static org.springframework.security.core.userdetails.User.builder;
 public class AuthService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
     public String signin(Login login) {
@@ -36,6 +43,7 @@ public class AuthService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new InvalidSigninInformation());
 
@@ -44,13 +52,32 @@ public class AuthService implements UserDetailsService {
         }
 
         log.info("user :: = {}", user.getEmail());
-        log.info("user :: = {}", user.getId());
-        log.info("user :: = {}", user.getPassword());
+        log.info("user.getPassword() = {}", user.getPassword());
 
         return builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
                 .authorities(Collections.emptyList())
                 .build();
+    }
+
+    @Transactional
+    public TokenInfo signin2(Login login) {
+        log.info("tokenInfo = testset");
+        // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
+        // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(login.getEmail(), login.getPassword());
+
+        // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 매서드가 실행될 때 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        log.info("authentication = {}", authentication);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        log.info("tokenInfo = {}", tokenInfo);
+
+        return tokenInfo;
     }
 }
