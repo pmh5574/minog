4dafabda54e25645d45d7da5e67c19f25eb3e628
pmh5574 <g_a_m_e_@naver.com>
2023-10-05 11:36:18 +0900

    231004 application.yml 삭제

--- src/main/java/com/minog/minog/MinogApplication.java
+++ src/main/java/com/minog/minog/MinogApplication.java
@@ -1,14 +1,19 @@
 package com.minog.minog;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+//@EnableJpaAuditing // JPA Auditing 활성화
 //@EnableConfigurationProperties(AppConfig.class)
 @SpringBootApplication
 public class MinogApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,";
 
 	public static void main(String[] args) {
-		SpringApplication.run(MinogApplication.class, args);
+		new SpringApplicationBuilder(MinogApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
