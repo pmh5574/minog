b72bda998bd3d534c8171d112174be21028c72f5
pmh5574 <g_a_m_e_@naver.com>
2023-10-05 14:21:54 +0900

    231005 application.yml git에 안올리고 하는 법 찾는 중..

--- src/main/java/com/minog/minog/MinogApplication.java
+++ src/main/java/com/minog/minog/MinogApplication.java
@@ -8,12 +8,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class MinogApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,";
+			+ "classpath:application.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(MinogApplication.class)
 				.properties(APPLICATION_LOCATIONS)
 				.run(args);
+
 	}
 
 }
